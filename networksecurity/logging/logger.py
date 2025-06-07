82ebc45c0c01307ce4644f21832651a85802fbad
hariomb845 <hariomb845@gmail.com>
2025-06-07 20:07:03 +0530

    Data Ingestion done

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -2,15 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path, LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s]%(lineo)d%(name)s-%(levelname)s-%(message)s",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s - line: %(lineno)d",
     level=logging.INFO,
 )
+
